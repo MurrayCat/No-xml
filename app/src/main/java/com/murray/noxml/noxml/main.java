7594abcc5263744d0b215325a71ea907d2af0f61
MurrayCat <murray.catto@yahoo.co.za>
2014-11-05 17:45:49 +0200

    java no xml adapter

--- app/src/main/java/com/murray/noxml/noxml/main.java
+++ app/src/main/java/com/murray/noxml/noxml/main.java
@@ -1,20 +1,28 @@
 package com.murray.noxml.noxml;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.GridLayout.LayoutParams;
 
 import android.graphics.Color;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by murray on 2014/11/05.
@@ -43,20 +51,11 @@ public class main extends Activity {
         setContent(gv);
         setGVAdapter(gv);
 
-        
+
     }
     public void setGVAdapter(GridView gv){
-        List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
-        for(int i=0;i<colors.length;i++) {
-            HashMap<String, String> hm = new HashMap<String, String>();
-            hm.put("colors", colors[i]);
-            hm.put("images", Integer.toString(images[i]));
-            aList.add(hm);
-        }
-        String[] from = { "colors","images"};
-        int[] to = { R.id.colors,R.id.images};
-        SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), aList, R.layout.custom_item, from, to);
-        gv.setAdapter(adapter);
+
+        gv.setAdapter(new MyAdapter(this, colors));
     }
     public void setContent(GridView gv){
         int gvID = 100;
@@ -70,6 +69,60 @@ public class main extends Activity {
         gv.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
         gv.setGravity(Gravity.CENTER);
         setContentView(gv);
+    }
+
+
+
+
+    private class MyAdapter extends ArrayAdapter<String> {
+
+        public MyAdapter(Context context, String[] strings) {
+            super(context, -1, -1, strings);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LinearLayout listLayout = new LinearLayout(main.this);
+            listLayout.setLayoutParams(new AbsListView.LayoutParams(
+                    AbsListView.LayoutParams.WRAP_CONTENT,
+                    AbsListView.LayoutParams.WRAP_CONTENT));
+            listLayout.setId(5000);
+
+            TextView listText = new TextView(main.this);
+            listText.setId(5001);
+
+            ImageView imgView = new ImageView(main.this);
+            listText.setId(5002);
+
+            listLayout.addView(listText);
+            listLayout.addView(imgView);
+
+            listText.setText(super.getItem(position));
+            Drawable background;
+            switch (position){
+
+                case 0:
+                     background = getResources().getDrawable(R.drawable.ic_red);
+                    imgView.setBackground(background);
+                    break;
+                case 1:
+                     background = getResources().getDrawable(R.drawable.ic_blue);
+                    imgView.setBackground(background);
+                    break;
+                case 2:
+                     background = getResources().getDrawable(R.drawable.ic_green);
+                    imgView.setBackground(background);
+                    break;
+                case 3:
+                     background = getResources().getDrawable(R.drawable.ic_purple);
+                    imgView.setBackground(background);
+                    break;
+
+            }
+
+            return listLayout;
+        }
     }
 
 }
